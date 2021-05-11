0423fb51bc0651a9c67575c8fb229f0be03f4e6b
esiochi <emsiochi@up.edu.ph>
2021-05-11 23:35:44 +0800

    feat: filter side bar w/o change handling yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Container from '@material-ui/core/Container';
+import FilterSideBar from "./Components/FilterSideBar";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <FilterSideBar/>
     </div>
   );
 }
