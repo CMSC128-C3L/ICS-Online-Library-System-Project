b3ba0650d2fec5f2d051bff885365b8ba403850a
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-05-11 22:10:36 +0800

    feat: footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,13 +1,41 @@
+import CategoryColumn from './CategoryColumn'
 import './Footer.css'
-import { Component } from 'react'
 
-class Footer extends Component {
-    render() {
-        return(
-            <div className="footer-container">
+function Footer(props) {
+    return(
+        <div className="footer-container">
+            <div className="placeholder-ilib-logo">iLib Name</div>
+            <div className="footer-row">
+                <CategoryColumn content={searchprop}></CategoryColumn>
+                <CategoryColumn content={toolsprop}></CategoryColumn>
+                <CategoryColumn content={contactprop}></CategoryColumn>
+                <CategoryColumn content={socialprop}></CategoryColumn>
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+const searchprop = {
+    title: "Search",
+    isNav: true,
+    links: ["Home", "Books", "Journals", "SP", "Thesis"]
+}
+
+const toolsprop = {
+    title: "Tools",
+    isNav: true,
+    links: ["Advanced Search", "Feature 1", "Feature 2"]
+}
+
+const contactprop = {
+    title: "Contact Us",
+    isNav: true,
+    links: ["Support", "About Us"]
+}
+
+const socialprop = {
+    title: "Social Media",
+    isNav: false
 }
 
 export default Footer
