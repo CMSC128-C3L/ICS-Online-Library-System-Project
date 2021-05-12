6c57502e5c310bb9589aafc17779d5d17e639762
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-05-12 22:43:25 +0800

    chore: added sample image

--- frontend/src/components/cards/Card.js
+++ frontend/src/components/cards/Card.js
@@ -1,10 +1,11 @@
 import './Card.css'
+import sample from './images/sample.jpg'
 
 function Card(props){
     return (
         <div className="card card-container flex-column">
             <div className="card-header text">{props.content.header}</div>
-            <div className="placeholder-image"></div>
+            <img src={sample} alt="sample"></img>
             <div className="article-title text">{props.content.title}</div>
             <a href="test">
                 <button className="view-button text">VIEW</button>
