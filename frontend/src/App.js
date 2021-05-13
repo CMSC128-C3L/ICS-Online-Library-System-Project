a4dd4d5e68cbfeed9874aaccad1843ef93f25091
jtogbinar <jtogbinar@up.edu.ph>
2021-05-13 15:50:41 +0800

    chore: clean code feat: incorporate footer from alwin's code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
 import FilterSideBar from "./Components/filter_sidebar/FilterSideBar";
 import Navbar from "./Components/navigation_bar/Navbar";
 import SearchResults from "./Components/search_results/SearchResults";
+import Footer from './Components/footer/Footer';
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <FilterSideBar/>
       <SearchResults/>
+      <Footer/>
     </div>
   );
 }
