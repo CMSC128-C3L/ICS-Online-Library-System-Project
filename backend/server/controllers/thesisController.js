2f8576da02734e6349af0e76e72ea6a11b77022f
samueljade <sbferrer3@up.edu.ph>
2021-05-20 02:10:12 +0800

    chore: removed sample thesis route

--- backend/server/controllers/thesisController.js
+++ backend/server/controllers/thesisController.js
@@ -1,7 +1,6 @@
 const Thesis = require('../models/Thesis.js');
 
 module.exports = {
-  sample,
   getAll,
   getOne,
   create,
@@ -68,34 +67,6 @@ async function deleteOne(req, res) {
     res.status(200).send();
   } catch(error) {
     console.log(error);
-    res.status(500).send();
-  }
-}
- 
-async function sample(req, res) {
-  try {
-    // sample data only!, must get from db in actual 
-    const thesis = {
-      title: 'Parallelizing unit test execution on GPU',
-      authors: ['Taghreed Bagies'],
-      year: 2020,
-      abstract:'Software testing is an important stage of the software development life cycle. However, the test execution is time-consuming and costly.'
-    }
-
-    // checks the user type
-    if(req.user.type === 3) {
-      // If user type is 3 (guest/not logged in)
-      // get the info from sample data
-      const {title, authors, year} = thesis;
-      // send the info as an object
-      res.status(200).send({title, authors, year});
-    } else {
-      // Since the user is at least 2(student), send everything including abstract
-      res.status(200).send(thesis);
-    }
-    // If there are more limitations, change what to send back per user type
-  } catch(error) {
-    // console.log(error)
     res.status(500).send();
   }
 }
