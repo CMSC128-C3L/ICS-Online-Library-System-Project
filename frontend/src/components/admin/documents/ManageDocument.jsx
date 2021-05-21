fbcc326dbf61455bbf16f28022fe3989ab8972df
jtogbinar <jtogbinar@up.edu.ph>
2021-05-21 09:10:46 +0800

    fix: show one data only on edit page

--- frontend/src/components/admin/documents/ManageDocument.jsx
+++ frontend/src/components/admin/documents/ManageDocument.jsx
@@ -10,14 +10,26 @@ import TagsInput from './TagsInput';
 import './DocumentCard.css';
 
 function ManageDocument(props) {
-//initialize collection of document to an empty array
-const [document, setDocument] = useState([])
+//initialize collection of document details to an empty array
+const [documentThumbnail, setDocumentThumbnail] = useState("");
+const [documentTitle, setDocumentTitle] = useState("");
+const [documentAuthor, setDocumentAuthor] = useState("");
+const [documentPublished, setDocumentPublished] = useState("");
+const [documentPublisher, setDocumentPublisher] = useState("");
+const [documentISBN, setDocumentISBN] = useState("");
+const [documentDescription, setDocumentDescription] = useState("");
 
     const getDocument = async() =>{
         try{
-            const documents = await axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline");
-            console.log(documents.data)
-            setDocument(documents.data)
+            const documents = await axios.get("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline");
+            console.log(documents);
+            setDocumentThumbnail(documents.data[0].image_link);
+            setDocumentTitle(documents.data[0].name);
+            setDocumentAuthor(documents.data[0].brand);
+            setDocumentPublished(documents.data[0].id);
+            setDocumentPublisher(documents.data[0].product_type);
+            setDocumentISBN(documents.data[0].id);
+            setDocumentDescription(documents.data[0].description);
         }catch(e){
             console.log(e)
         }
@@ -33,49 +45,45 @@ const [document, setDocument] = useState([])
     return (
             <div className="edit-main-content">
                 {
-                    document.map(document=>{
-                        return (
-                            <div>
-                                <div className='document-card-flex-row'>
-                                    <div className='image-card-container-flex-column' >
-                                    <img src={document.image_link} alt="" className="thumbnail"></img>
-                                    </div>
-                                    
-                                    <div className='document-card-container document-card-flex-column' key={document.id}>
-                                    <DocumentCard
-                                        title={document.name}
-                                        author={document.brand} 
-                                        yearPublished={document.id}
-                                        publisher={document.product_type}
-                                        docISBN={document.id}
-                                    />
-                                    <TagsInput selectedTags={selectedTags}/>
-                                    </div>
-
-                                    <div className='document-card-container button-card-flex-column'>
-                                        <button className={classes.textStyle}><GetAppIcon className={classes.iconStyle}/> DOWNLOAD PDF</button>
-                                        <button className={classes.textStyle}><EditIcon className={classes.iconStyle}/>UPDATE PDF</button>
-                                    </div>
-                                </div>
-
-                                <div className="description-section">
-                                    <h2>DESCRIPTION</h2>
-                                    <Box className={classes.boxStyle}>
-                                        {document.description}
-                                        {document.description}
-                                        {document.description}
-                                        {document.description}
-                                        {document.description}
-                                        {document.description}
-                                    </Box>
-                                    
-                                    <div className = "button-right">
-                                    <button aria-label="save" style={{ backgroundColor: '#47ABD8', 'borderRadius':'10vh', width:'10vh', height:'10vh'}}><SaveIcon style={{ color: 'black' }}/></button>
-                                    </div>
-                                </div>
+                    <div>
+                        <div className='document-card-flex-row'>
+                            <div className='image-card-container-flex-column' >
+                            <img src={documentThumbnail} alt="" className="thumbnail"></img>
                             </div>
-                        )
-                    })
+                            
+                            <div className='document-card-container document-card-flex-column' key={documentISBN}>
+                            <DocumentCard
+                                title={documentTitle}
+                                author={documentAuthor} 
+                                yearPublished={documentPublished}
+                                publisher={documentPublisher}
+                                docISBN={documentISBN}
+                            />
+                            <TagsInput selectedTags={selectedTags}/>
+                            </div>
+
+                            <div className='document-card-container button-card-flex-column'>
+                                <button className={classes.textStyle}><GetAppIcon className={classes.iconStyle}/> DOWNLOAD PDF</button>
+                                <button className={classes.textStyle}><EditIcon className={classes.iconStyle}/>UPDATE PDF</button>
+                            </div>
+                        </div>
+
+                        <div className="description-section">
+                            <h2>DESCRIPTION</h2>
+                            <Box className={classes.boxStyle}>
+                                {documentDescription}
+                                {documentDescription}
+                                {documentDescription}
+                                {documentDescription}
+                                {documentDescription}
+                                {documentDescription}
+                            </Box>
+                            
+                            <div className = "button-right">
+                            <button aria-label="save" style={{ backgroundColor: '#47ABD8', 'borderRadius':'10vh', width:'10vh', height:'10vh'}}><SaveIcon style={{ color: 'black' }}/></button>
+                            </div>
+                        </div>
+                    </div>
                 }
             </div>
     )
