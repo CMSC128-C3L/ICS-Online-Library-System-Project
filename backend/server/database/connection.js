cc6485f0aa58915a64bd629bfa7cde0f9d779420
NeonPikachu17 <msbernabe1@up.edu.ph>
2021-05-22 10:49:59 +0800

    fix: connection to cloud database

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+const uri = "mongodb+srv://admin:admin@database.61lmf.mongodb.net/db?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true
-});
+})
+
+.then(() => {
+  console.log("MongoDB Connected…")
+})
+.catch(err => console.log(err))
