3320c78cb66c6428a51f5911b3b053a30cfb6ba3
ckrp120 <kpcadacio1@up.edu.ph>
2021-05-25 21:44:03 +0800

    feat: add userContext to access user credentials

--- frontend/src/components/header_user/Header.js
+++ frontend/src/components/header_user/Header.js
@@ -1,12 +1,34 @@
-import '../header_home/Header.css'
+import '../header_user/Header.css'
 import {useHistory} from 'react-router-dom'
+import {useContext} from 'react'
 import Button from '@material-ui/core/Button'
 import Logout from '../login/Logout'
+import { UserContext } from '../user/UserContext';
+import Login from '../login/Login'
 
 function Header({name}) {
     const history = useHistory();
+    const {loggedUser, setLoggedUser} = useContext(UserContext)
 
+    const renderComponentsBasedOnState = () =>{
+        if(JSON.stringify(loggedUser) === '{}'){
+            return(
+                <div className="Header-container-right" >
+                    <Login/>
+                     <Button className="Button" onClick={() => history.push('/support')}>Support</Button>
+                </div>
+            )
+        }
 
+        else{
+            return(
+                  <div className="Header-container-right" >
+                    <Button className="Button">{loggedUser.name}</Button>
+                    <Logout/>
+                </div>
+            )
+        }
+    }
     return(
         <div>
             <div className="Logo-area">
@@ -19,11 +41,8 @@ function Header({name}) {
                     <Button className="Button" onClick={() => history.push('/tools')}>Tools</Button>
                 </div>
             
-                <div className="Header-container-right" >
-                    
-                    <Button className="Button" onClick={() => history.push('/support')}>{name}</Button>
-                    <Logout/>
-                </div>  
+               {renderComponentsBasedOnState()}
+                
             </div>
                      
 
--- frontend/src/pages/GuestHome.jsx
+++ frontend/src/pages/GuestHome.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import Header from '../components/header_home/Header'
+import React, { useContext, useEffect } from 'react'
+import Header from '../components/header_user/Header'
 import Search from '../components/search/Search'
 import CardRow from '../components/cards/CardRow'
+import {UserContext} from '../components/user/UserContext'
+function GuestHome() {
+    const {loggedUser, setLoggedUser} = useContext(UserContext) 
 
-export default class GuestHome extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Search/>
-                <CardRow/>
-            </div>
-        )
-    }
+    //check to see if user is still logged in
+        useEffect(() => {
+            console.log('user', loggedUser)
+        }, [])
+
+    return (
+        <div>
+            <Header/>
+            <Search/>
+            <CardRow/>
+        </div>
+    )
+
+
+    
 }
+
+export default GuestHome
