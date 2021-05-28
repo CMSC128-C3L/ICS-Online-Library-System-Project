046e8cd1ac1b3c2001f06b07590cf76d5d20a68c
RalfBautista <rpbautista4@up.edu.ph>
2021-05-28 13:43:21 +0800

    feat: add required attributes to the schema of Sp

--- backend/server/models/Sp.js
+++ backend/server/models/Sp.js
@@ -2,19 +2,23 @@ const mongoose = require("mongoose");
 
 const spSchema= new mongoose.Schema({
     id: Number,
-    type: String,
-    title: String,
+    type: String, 
+    title: {
+        type: String,
+        required: true,
+    },
     author: [String],
     adviser: [String],
-    pub_date: Date,
+    pub_date: {
+        type: Date,
+        required: true,
+    },
     abstract: String,
     topic: [String]
 
 },{
     collection: 'Papers'
 }
-
-
 );
 
 const Sp= mongoose.model("Special Problem",spSchema);
