15fc62fb855b9554cbdc977b497171c62a5fb9ba
RalfBautista <rpbautista4@up.edu.ph>
2021-05-28 13:41:43 +0800

    feat: Add update and delete sp controllers

--- backend/server/controllers/spController.js
+++ backend/server/controllers/spController.js
@@ -52,19 +52,29 @@ async function create(req, res) {
 }
 
 async function deleteSp(req, res) {
-    try{
-
-    }catch(err){
-        
-    }
-    
+    try {
+    let _id = req.params.id;
+    let sp = await Sp.findOneAndDelete({_id});
+    if(sp!=null)  res.status(200).send(sp);
+    res.status(404).send({message:"Sp not found"});
+      } catch(err) {
+        res.status(500).send();
+      }
 }
 
 async function update(req, res) {
     try{
+        let _id = req.params.id;
+        let query= {_id};
+       
+        let sp = await Sp.findOneAndUpdate({_id}, req.body, {upsert: true, new: true}); //create the updated sp or new sp if the filter cannot find the sp 
+        if(sp!=null)  res.status(200).send(sp);
 
-    }catch(err){
+        return res.status(404).send();
+       
+        }catch(err){
+            res.status(400).send({message:"Error"});
+        }
         
-    }
     
 }
