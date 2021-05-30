ab086767236110cabcd889748d4806e3a2a69363
jtogbinar <jtogbinar@up.edu.ph>
2021-05-31 06:55:31 +0800

    fix: add logo and title to footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,10 +1,14 @@
 import CategoryColumn from './CategoryColumn'
+import icsLogo from '../../assets/ics_logo.png'
 import './Footer.css'
 
 function Footer(props) {
     return(
         <div className="footer-container">
-            <div className="placeholder-ilib-logo">eyeCS</div>
+            <div className="placeholder-ilib-logo">
+            <img src={icsLogo} alt="logo" className="ics_icon"/> 
+            EyeCS: Window to Knowledge
+            </div>
             <div className="footer-row">
                 <div className="category-row">
                     <CategoryColumn content={searchprop}></CategoryColumn>
