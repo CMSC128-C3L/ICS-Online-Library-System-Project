2c96274281cf2bbc903c0c236228f6296bb95410
cvpua <cvpua@up.edu.ph>
2021-06-03 21:17:50 +0800

    Implement initial authorSummary function

--- backend/server/controllers/authorSummary.js
+++ backend/server/controllers/authorSummary.js
@@ -2,6 +2,10 @@ const Book = require("../models/Book.js");
 const Sp = require('../models/Sp.js');
 const Journal= require('../models/Journal.js');
 const Thesis = require('../models/Thesis.js');
+const fs = require("fs");
+const PDF = require("pdfkit");
+const doc = new PDF();
+
 
 module.exports = {
     getSummary
@@ -9,14 +13,55 @@ module.exports = {
 
 async function getSummary(req, res) {
     try {
-        const author = req.params.author;
+        const author = req.body.author;
+        try {
+            const book = await Book.find({author}); // get all of the books
+            const thesis = await Thesis.find({type:'Thesis',author}); // get all of the thesis
+            const sp = await Sp.find({type:"Special Problem",author }); // get all of the sp
+            
+            doc.pipe(fs.createWriteStream('file.pdf'));
+            doc.text(author);
+            doc.text(" ");
+            book.map(item => {
+                doc.text(item.title);
+                doc.text(item.year);
+                doc.text(item.type);
+                item.topic.map(tpc => {
+                    doc.text(tpc);
+                });
+                doc.text(" ");
+            });
+            sp.map(item => {
+                doc.text(item.title);
+                const editedDate = String(item.pub_date);
+                const strippedDate = editedDate.split(" ");
+                doc.text(strippedDate[1] + " " + strippedDate[2] + ", " + strippedDate[3]);
+                doc.text(item.type);
+                item.topic.map(tpc => {
+                    doc.text(tpc);
+                });
+                doc.text(" ");
+            });
+            thesis.map(item => {
+                doc.text(item.title);
+                const editedDate = String(item.pub_date);
+                const strippedDate = editedDate.split(" ");
+                doc.text(strippedDate[1] + " " + strippedDate[2] + ", " + strippedDate[3]);
+                doc.text(item.type);
+                item.topic.map(tpc => {
+                    doc.text(tpc);
+                });
+                doc.text(" ");
+            });
+            doc.end();
 
-        // query
+            res.status(200).send("ok");     // respond with the array of books 
+        }catch (err) {
+            res.status(400).send({message:"error"});
+        }
 
-        // create pdf
-
-        res.status(200);
-        res.send({author:author});
+        // res.status(200);
+        // res.send({author});
 
     } catch (err) {
         console.log(err);
