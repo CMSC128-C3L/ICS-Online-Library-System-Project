caddf62faf13392921529e6fba89af8344e6f26a
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-06-07 02:15:36 +0800

    chore: EditCards and CardRow refactor

--- frontend/src/components/cards/CardRow.js
+++ frontend/src/components/cards/CardRow.js
@@ -1,28 +1,32 @@
 import Card from './Card'
 
-function CardRow(props){
+function CardRow({advisory, featured, icsnews}){
     return (
         <div className='flex-row'>
-            <Card content={advisoryprop}/>
-            <Card content={announcementsprop}/>
-            <Card content={icsnewsprop}/>
+            <Card content={advisory? advisory : advisoryprop}/>
+            <Card content={featured? featured : featuredprop}/>
+            <Card content={icsnews? icsnews : icsnewsprop}/>
         </div>
     )
 }
 
+// Default values for props while db not set
 const advisoryprop = {
     header : "Advisory",
-    title : "Lorem ipsum dolor sit amet"
+    title : "Lorem ipsum dolor sit amet",
+    image : ""
 }
 
-const announcementsprop = {
+const featuredprop = {
     header : "Featured",
-    title : "Lorem ipsum dolor sit amet"
+    title : "Lorem ipsum dolor sit amet",
+    image : ""
 }
 
 const icsnewsprop = {
     header : "ICS News",
-    title : "Lorem ipsum dolor sit amet"
+    title : "Lorem ipsum dolor sit amet",
+    image : ""
 }
 
 export default CardRow
--- frontend/src/components/edit_cards/EditCards.jsx
+++ frontend/src/components/edit_cards/EditCards.jsx
@@ -1,46 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import CardRow from '../cards/CardRow'
 import './EditCards.css'
 import EditIcon from '@material-ui/icons/Edit';
 
 function EditCards(props){
+    const history = useHistory();
+
+    // set default card state
+    const [cards, setCards] = useState({
+        advisory: {
+            header: "Advisory",
+            title: "Lorem ipsum dolor sit amet",
+            image: ""
+        },
+        featured: {
+            header: "Featured",
+            title: "Lorem ipsum dolor sit amet",
+            image: ""
+        },
+        icsnews: {
+            header: "ICS News",
+            title: "Lorem ipsum dolor sit amet",
+            image: ""
+        }
+    })
+
+    // Go to edit x page on click of edit button
+    const onClickEditCard = (header) => {
+        history.push(`/edit${cleanWhitespace(header)}`)
+    }
+
+    // Remove whitespaces in string
+    const cleanWhitespace = (string) => {
+        return string.replace(/\s/g, "");
+    }
+
     return (
         <div className="page-container">
             <div className="btn-cont">
-                <div className="edit-btn" onclick={clickFirst}><EditIcon/></div>
-                <div className="edit-btn" onclick={clickSecond}><EditIcon/></div>
-                <div className="edit-btn" onclick={clickThird}><EditIcon/></div>
+                <div className="edit-btn" onClick={() => onClickEditCard(cards.advisory.header)}><EditIcon/></div>
+                <div className="edit-btn" onClick={() => onClickEditCard(cards.featured.header)}><EditIcon/></div>
+                <div className="edit-btn" onClick={() => onClickEditCard(cards.icsnews.header)}><EditIcon/></div>
             </div>
-            <CardRow/>
+            
+            <CardRow
+                advisory={cards.advisory}
+                featured={cards.featured}
+                icsnews={cards.icsnews}
+            />
         </div>
     )
-}
-
-const clickFirst = () => {
-
-}
-
-const clickSecond = () => {
-    
-}
-
-const clickThird = () => {
-    
-}
-
-const advisoryprop = {
-    header : "Advisory",
-    title : "Lorem ipsum dolor sit amet"
-}
-
-const announcementsprop = {
-    header : "Featured",
-    title : "Lorem ipsum dolor sit amet"
-}
-
-const icsnewsprop = {
-    header : "ICS News",
-    title : "Lorem ipsum dolor sit amet"
 }
 
 export default EditCards
