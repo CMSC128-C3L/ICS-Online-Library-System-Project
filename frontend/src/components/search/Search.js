b584a31ae4858955ae20ae59b413a8cbe3924241
ckrp120 <kpcadacio1@up.edu.ph>
2021-06-07 20:21:22 +0800

    fix: make homepage search work again

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -2,7 +2,7 @@ import './Search.css'
 import React, {useState, useContext} from 'react'
 import {useHistory} from 'react-router-dom'
 import SearchContext from '../search_results/SearchContext'
-
+import HomeCategoryChecklist from './HomeCategoryChecklist';
 export const ACTIONS = {
   updateQuery: 'UPDATE_QUERY',
   updateCategory: 'UPDATE_CATEGORY',
@@ -17,7 +17,7 @@ function Search(props) {
     const [query, setQuery] = useState('');
     const history = useHistory();
     const searchContext = useContext(SearchContext)
-
+    const category = {'name': 'Category', 'list': ["Books", "Special Problems", "Theses"]};
     const handleChange = (event) =>{
 		setQuery(event.target.value);
     };
@@ -42,25 +42,10 @@ function Search(props) {
             <input onKeyDown={enterSubmit} onChange={handleChange} onSubmit={handleSubmit} className="App-search-bar" type="text" placeholder="Search..."/>
             
             <br/>
+
             <div className="Search-container">
-                <div>
-                    <input id="books-check" type="checkbox" name="search-type" value="search-books"/>
-                    <label className="Check-label" htmlFor="books-check">  Books</label>
-                </div>
-                <div>
-                    <input id="journal-check" type="checkbox" name="search-type" value="search-journal"/>
-                    <label className="Check-label" htmlFor="journal-check">  Journals</label>
-                </div>
-                <div>
-                    <input id="sp-check" type="checkbox" name="search-type" value="search-sp"/>
-                    <label className="Check-label" htmlFor="sp-check">  SP</label>
-                </div>
-                <div>
-                    <input id="thesis-check" type="checkbox" name="search-type" value="search-thesis"/>
-                    <label className="Check-label" htmlFor="thesis-check">  Thesis</label>
-                </div>                  
-            </div>
-                
+            <HomeCategoryChecklist list={category.list} action={ACTIONS.updateCategory} />
+            </div>  
             <br/>
             <button className="Search-btn" onClick={handleSubmit}>Search</button>
         </form>
