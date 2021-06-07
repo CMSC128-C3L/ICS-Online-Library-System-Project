6af0d3827986755f3f23cb11adc5f44f3ae5e403
aaronalba <aaalba1@up.edu.ph>
2021-06-07 17:46:52 +0800

    feat: add multer middleware to HomeAdvController
    This will handle the thumbnail uploads for the Home Advisories.

--- backend/server/controllers/homeAdvisoryController.js
+++ backend/server/controllers/homeAdvisoryController.js
@@ -9,3 +9,16 @@
 
 
 //
+const multer = require('multer');
+const path = require("path");
+
+// This will be used in router.js as middleware for the thumbnail of the home advisories
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, path.join(__dirname, '../uploads/home_advisory'));
+    },
+    filename: function(req, file, callback) {
+        callback(null, "s-" + Date.now() + "-" + file.originalname);
+    }
+});
+const uploads = multer({ storage }).single('home_advisory');
