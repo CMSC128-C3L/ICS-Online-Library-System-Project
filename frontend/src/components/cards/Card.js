dc5c628de280799a1c3ecbdfc46735942150dd0d
esiochi <emsiochi@up.edu.ph>
2021-06-10 19:02:25 +0800

    fix: show image in card

--- frontend/src/components/cards/Card.js
+++ frontend/src/components/cards/Card.js
@@ -1,11 +1,10 @@
 import './Card.css'
-import sample from '../../assets/sample.jpg'
 
 function Card(props){
     return (
         <div className="card card-container flex-column">
             <div className="card-header text">{props.content.header}</div>
-            <img className="img" src={sample} alt="sample"></img>
+            <img className="img" src={props.content.image} alt="sample"></img>
             <div className="article-title text">{props.content.title}</div>
             <div className="article-title text-regular">{props.content.description}</div>
             <a href="test">
