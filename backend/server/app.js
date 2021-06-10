630794e1977b51ea19be499fabba0bd68fb80afe
aaronalba <aaalba1@up.edu.ph>
2021-06-10 16:24:31 +0800

    fix: missing '/' in end of root of static folder

--- backend/server/app.js
+++ backend/server/app.js
@@ -9,7 +9,7 @@ app.use(router);
 
 // statically serve the books and advisory image files
 const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'uploads/books')));
-app.use('/static', express.static(path.join(__dirname, 'uploads/home_advisory')));
+app.use('/static/', express.static(path.join(__dirname, 'uploads/books')));
+app.use('/static/', express.static(path.join(__dirname, 'uploads/home_advisory')));
 
 module.exports = app;
