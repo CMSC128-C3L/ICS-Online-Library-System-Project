26520ed3d4c8b1cec8ee3cc12b3d81296d3b0123
esiochi <emsiochi@up.edu.ph>
2021-06-12 14:01:38 +0800

    fix: view btn now opens tab based on link

--- frontend/src/components/cards/Card.js
+++ frontend/src/components/cards/Card.js
@@ -7,7 +7,7 @@ function Card(props){
             <img className="img" src={props.content.image} alt="sample"></img>
             <div className="article-title text">{props.content.title}</div>
             <div className="article-title text-regular">{props.content.description}</div>
-            <a href="test">
+            <a href={props.content.link} target="_blank">
                 <button className="view-button">VIEW</button>
             </a>
         </div>
