532eb9bdbcb27437d3a3dd6643ea8b682db5ef9d
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-06-13 13:14:31 +0800

    chore: adjust card ui, fix: CardEditingPage warning

--- frontend/src/components/cards/Card.js
+++ frontend/src/components/cards/Card.js
@@ -2,15 +2,15 @@ import './Card.css'
 
 function Card(props){
     return (
-        <div>
+        <div className="margin-bot-2">
             <div className="card-header text">{props.content.header}</div>
             <div className="card-container">
                 <div className="flex-column">
                     <img className="card-img" src={props.content.image} alt="sample"></img>
-                    <div className="article-title text">{props.content.title}</div>
-                    <div className="article-description text">{props.content.description}</div>
+                    <div className="article-title opensans">{props.content.title}</div>
+                    <div className="article-description montserrat">{props.content.description}</div>
                     <a href={props.content.link} target="_blank" rel="noreferrer">
-                        <button className="view-button">VIEW</button>
+                        <button className="opensans view-button">VIEW</button>
                     </a>
                 </div>
             </div>
